bd9f0745d34863ea1cff758a7f71687d0d990c62
TejasRaghuram <tejas.raghuram101@gmail.com>
2024-10-30 00:32:26 -0700

    Created Funds Form

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,7 +1,17 @@
+import "./../styles/Donate.css";
+
 function Donate() {
     return (
-        <div>
-            <p>This is the Donate Page.</p>
+        <div id="donate">
+            <h1 id="donate-header">Donate Towards a Cause</h1>
+            <h2 id="funds-header">Add Funds</h2>
+            <form>
+                <input class="input" type="number" placeholder="Credit Card Number"/>
+                <br/>
+                <input class="input" type="number" placeholder="Money Amount"/>
+                <br/>
+                <input id="submit" type="submit"/>
+            </form>
         </div>
     );
 }
